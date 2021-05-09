5d880fadb6573d9cc19d5c3eb3eae84a368b7674
Santos Rosati <srosati@itba.edu.ar>
2021-05-09 09:02:40 -0300

    Connected profile to API

--- src/stores/profileStore.js
+++ src/stores/profileStore.js
@@ -1,10 +1,35 @@
+import { Api } from "@/api/api";
+
 const profileStore = {
-    name: 'Nicolas Gutierrez',
-    email: 'nico.gutierrez@gmail.com',
-    gender: 'MALE',
+    name: 'Santos Rosati',
+    email: 'srosati@itba.edu.ar',
+    gender: 'male',
     age: 20,
-    height: 180,
-    weight: 90
+    height: 173,
+    weight: 67,
+    update: async () => {
+        const result = await Api.get(Api.baseUrl+'/users/current', true, null);
+        console.log(result);
+        if (result.code === 200) {
+            const {firstName, email, gender, age, height, weight} = result.body;
+            profileStore.name = firstName;
+            profileStore.email = email;
+            profileStore.gender = gender;
+            profileStore.age = age;
+            profileStore.height = height;
+            profileStore.weight = weight;
+        }
+    },
+    modify: async () => {
+        const data = {
+            firstName: profileStore.name,
+            age: profileStore.age,
+            gender: profileStore.gender,
+            height: profileStore.height,
+            weight: profileStore.weight
+        };
+        await Api.put(Api.baseUrl+'/users/current', true, data, null);
+    }
 }
 
 export default profileStore;
