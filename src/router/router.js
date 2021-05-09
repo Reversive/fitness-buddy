f3e35c2a714a0fd59790c28f2541faa3bdefdde8
Santos Rosati <srosati@itba.edu.ar>
2021-05-09 11:33:30 -0300

    Route to login if there is no user

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,36 @@
 import Vue from 'vue';
-import Router from 'vue-router'
+import Router from 'vue-router';
+import {Api} from '@/api/api'
 
 Vue.use(Router);
 
 const router = new Router({
     routes: [
         { path: '/', component: () => import('../views/Login'), name: 'login' },
-        { path: '/profile', component: () => import('../views/Profile') },
-        { path: '/personal-routines', component: () => import('../views/PersonalRoutines') },
-        { path: '/community-routines', component: () => import('../views/CommunityRoutines') },
-        { path: '/create-routine', component: () => import('../views/CreateRoutine')},
+        { path: '/profile', component: () => import('../views/Profile'), meta: {requiresLogin: true} },
+        { path: '/personal-routines', component: () => import('../views/PersonalRoutines'), meta: {requiresLogin: true} },
+        { path: '/community-routines', component: () => import('../views/CommunityRoutines'), meta: {requiresLogin: true} },
+        { path: '/create-routine', component: () => import('../views/CreateRoutine'), meta: {requiresLogin: true}},
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresLogin)) {
+            if(Api.token) {
+                next();
+            } else {
+                router.push({name: 'login'})
+                    .then(/*mostrar un error que diga que tenes que estar loggeado?*/)
+                    .catch((failure) => {
+                        if (Router.isNavigationFailure(failure, Router.NavigationFailureType.redirected)) {
+                            console.log('err');
+                        }
+                    });
+            }
+    } else {
+        next();
+    }
+});
+
 
 export default router;
