cd7cf1b5044e1a12a3c51056f14c38a717f90ae9
Santos Rosati <srosati@itba.edu.ar>
2021-05-11 17:23:04 -0300

    Added component routines

---
+++ src/components/Routines.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-relative fullWidth">
+    <v-row class="justify-space-between mb-10">
+      <RoutinePreviewCard v-for="routine in routines" :key="routine.id" :routine="routine" @onDeleteRoutine="deleteRoutine"/>
+    </v-row>
+    <v-btn v-if="showLoadMore" v-on:click="getRoutines" class="loadMoreBtn">Load More</v-btn>
+  </div>
+</template>
+<script>
+import RoutinePreviewCard from "@/components/RoutinePreviewCard";
+export default {
+  name: "Routines",
+  props: ['routines', 'showLoadMore' , 'getRoutines', 'deleteRoutine'],
+  components: {RoutinePreviewCard}
+}
+</script>
+
+<style scoped>
+.p-relative {
+  position: relative;
+}
+
+.loadMoreBtn {
+  position: absolute;
+  right: 0;
+  bottom: 0
+}
+
+.fullWidth {
+  width: 100%;
+}
+</style>
