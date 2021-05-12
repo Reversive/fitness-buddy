1fa2c08f9cce87b01b68211f8aa2012b9c84cce8
Santos Rosati <srosati@itba.edu.ar>
2021-05-12 17:42:36 -0300

    Search in community routines

--- src/views/CommunityRoutines.vue
+++ src/views/CommunityRoutines.vue
@@ -16,7 +16,8 @@
               <v-flex class="text-left">
                 <v-select
                     :items="searchFilter"
-                    v-model="sortBy"
+                    v-model="searchBy"
+                    v-on:change="resetSearch"
                     item-color="primary"
                     dark
                     label="SEARCH BY"
@@ -25,16 +26,48 @@
                     dense
                     outlined
                 />
-                <span>
+                <span v-if="searchBy === 'search' || searchBy === 'userId'">
                   <v-text-field
+                      id="searchInput"
                       class="d-inline-block ml-3 search-bar"
                       dark
                       label="SEARCH"
                       v-model="searchTerm"
+                      v-on:keyup="validateSearch"
+                      v-on:keyup.enter.prevent="searchRoutines"
+                      v-bind:error-messages="errorMessages"
                       append-icon="mdi-magnify"
                       out
                       color="white"
                       />
+                </span>
+                <span v-if="searchBy === 'categoryId'">
+                  <v-select
+                      :items="categories"
+                      v-model="searchTerm"
+                      item-color="primary"
+                      v-on:change="searchRoutines"
+                      dark
+                      label="SELECT"
+                      class="sort-by-px mt-5 ml-3 d-inline-block white--text"
+                      color="white"
+                      dense
+                      outlined
+                  />
+                </span>
+                <span v-if="searchBy === 'difficulty'">
+                  <v-select
+                      :items="difficulties"
+                      v-model="searchTerm"
+                      item-color="primary"
+                      v-on:change="searchRoutines"
+                      dark
+                      label="SELECT"
+                      class="sort-by-px mt-5 ml-3 d-inline-block white--text"
+                      color="white"
+                      dense
+                      outlined
+                  />
                 </span>
               </v-flex>
             </v-col>
@@ -68,15 +101,31 @@
 
 import {RoutineApi} from "@/api/routine";
 import Routines from "@/components/Routines";
+import {Api} from "@/api/api";
 
 export default {
   name: "CommunityRoutines",
   data: () => {
     return {
       routines: [],
-      sortBy: null,
+      searchBy: null,
       searchTerm: null,
-      searchFilter: ['AUTHOR', 'NAME'],
+      searchFilter: [
+        {text: 'CLEAR', value: 'none'},
+        {text: 'NAME', value: 'search'},
+        {text: 'AUTHOR', value: 'userId'},
+        {text: 'DIFFICULTY', value: 'difficulty'},
+        {text: 'CATEGORY', value: 'categoryId'}
+      ],
+      difficulties: [
+        {text: 'ROOKIE', value: 'rookie'},
+        {text: 'BEGINNER', value: 'beginner'},
+        {text: 'INTERMEDIATE', value: 'intermediate'},
+        {text: 'ADVANCED', value: 'advanced'},
+        {text: 'EXPERT', value: 'expert'}
+      ],
+      author: null,
+      search: false,
       orderItems: [
         {text: 'NAME', value: 'name'},
         {text: 'DATE', value: 'date'},
@@ -84,7 +133,9 @@ export default {
         {text: 'CATEGORY', value: 'categoryId'},
         {text: 'AUTHOR', value: 'userId'}
       ],
+      errorMessages: undefined,
       orderBy: 'name',
+      categories: [],
       filteredRoutines: [],
       model: null,
       page: 0,
@@ -92,14 +143,35 @@ export default {
       pageSize: 3
     }
   },
-  created() {
-    this.routines = [];
-    this.page = 0;
-    this.getRoutines();
+  mounted() {
+    Api.get(Api.baseUrl+'/categories', false, null).then((results) => {
+      this.categories = results.content.map((category) => {return {text: category.name, value: category.id}});
+      this.routines = [];
+      this.page = 0;
+      this.search = false;
+      this.getRoutines();
+    }).catch((e) => {
+      alert("Unable to retireve categories with error: " + e.message);
+    });
   },
   methods: {
+    resetAndGet() {
+      this.page = 0;
+      this.routines = [];
+      this.getRoutines();
+    },
     getRoutines() {
-      RoutineApi.get({page: this.page, size: this.pageSize, orderBy: this.orderBy}).then((results) => {
+      let req = {page: this.page, size: this.pageSize, orderBy: this.orderBy};
+      if (this.search) {
+        req[this.searchBy] = this.searchTerm;
+        if (this.searchBy === 'userId' && this.author) {
+          req[this.searchBy] = this.author;
+        }
+      }
+      RoutineApi.get(req).then((results) => {
+        if (this.searchBy === 'search' && results.content.length === 0) {
+          this.errorMessages = 'No routines found for given routine name';
+        }
         this.showLoadMore = !results.isLastPage;
         for (let i = 0; i < results.content.length; i++) {
           const result = results.content[i];
@@ -120,9 +192,56 @@ export default {
       });
     },
     orderRoutines() {
-      this.routines = [];
-      this.page = 0;
-      this.getRoutines();
+      this.resetAndGet();
+    },
+    searchRoutines() {
+      console.log(this.searchBy);
+      this.author = null;
+      if (!this.searchBy || this.searchBy === 'none' || this.searchBy.length === 0) {
+        this.search = false;
+        this.searchTerm = null;
+        return;
+      }
+
+      if ((this.searchBy === 'search' || this.searchBy === 'userId') && this.searchTerm && this.searchTerm.length < 3) {
+        this.search = false;
+        return;
+      }
+
+      if (this.searchBy === 'userId') {
+        const url = Api.baseUrl + '/users?' + new URLSearchParams({search: this.searchTerm});
+        console.log(url);
+        Api.get(url, true, null).then((result) => {
+          if (result.totalCount === 0) {
+            this.errorMessages = 'Please insert a valid username';
+            return;
+          }
+
+          this.author = result.content[0].id;
+          this.search = true;
+          this.resetAndGet();
+        }).catch((e) => {
+          console.log(e);
+        });
+        this.search = false;
+        return;
+      }
+
+      this.search = true;
+      this.resetAndGet();
+    },
+    resetSearch() {
+      this.search = false;
+      this.searchTerm = null;
+      this.resetAndGet();
+    },
+    validateSearch(e) {
+      const text = e.target.value;
+      if (text.length < 3) {
+        this.errorMessages = 'Please insert at least three characters';
+        return;
+      }
+      this.errorMessages = undefined;
     }
   },
   components: {
